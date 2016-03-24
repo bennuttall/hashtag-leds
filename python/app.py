f10c39e2a69b55129c3c7db4901b4028432399c3
Ben Nuttall <ben@bennuttall.com>
2016-03-24 13:26:23 +0000

    Add dictionary of hashtag -> LEDs

--- python/app.py
+++ python/app.py
@@ -2,8 +2,10 @@ from time import sleep
 from gpiozero import LED
 from twython import TwythonStreamer
 
-hashtag = '#yes'
-led = LED(18)
+hashtags = {
+    '#yes': LED(18),
+    '#no': LED(15),
+}
 
 # Twitter application authentication
 APP_KEY = 'erRilYZd8UzsXEFycmg'
@@ -16,10 +18,12 @@ class BlinkyStreamer(TwythonStreamer):
     def on_success(self, data):
         if 'text' in data:
             print(data['text'].encode('utf-8'))
-            led.on()
+            self.led.on()
             sleep(0.5)
-            led.off()
+            self.led.off()
 
 # Create streamer
-stream = BlinkyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-stream.statuses.filter(track=hashtag)
+for hashtag, led in hashtags.items():
+    stream = BlinkyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    stream.statuses.filter(track=hashtag)
+    stream.led = led
